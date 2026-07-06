da14f07aff5c7f7dc7ddc3bcffb3c2e373e59be1
j.anthonyselvan <janthonyselvan07@gmail.com>
2026-07-06 22:33:50 +0530

    Bubble sort

---
+++ src/Sort/BubbleSort.java
@@ -0,0 +1,27 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr= {6,3,9,8,1,2};
+        System.out.println(Arrays.toString(bubbleSort(arr)));
+    }
+    public static int[] bubbleSort(int[] arr){
+        boolean swapped = false;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j=1;j< arr.length-i;j++){
+                if (arr[j]<arr[j-1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j-1];
+                    arr[j-1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+        return arr;
+    }
+}
