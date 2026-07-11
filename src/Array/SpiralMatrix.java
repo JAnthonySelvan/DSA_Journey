b494927e73f8cbd90926177c40de3a813640e696
j.anthonyselvan <janthonyselvan07@gmail.com>
2026-07-11 18:53:17 +0530

    Spiral Matrix

--- src/Array/SpiralMatrix.java
+++ src/Array/SpiralMatrix.java
@@ -6,9 +6,9 @@ import java.util.List;
 public class SpiralMatrix {
     public static void main(String[] args) {
         int[][] matrix =
-               {{1,2,3},
-                {4,5,6},
-                {7,8,9}};
+               {{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12}};
         List<Integer> res = new ArrayList<>();
         int top=0;
         int left =0;
